7332156a4281922a28e7ef3b3700126ff44f200a
Jim Wang <jim.gs.wang@gmail.com>
2014-05-25 04:01:46 -0700

    add constructor

--- src/queue.js
+++ src/queue.js
@@ -1,8 +1,16 @@
 
-function Rq(options) {
+'use strict';
+
+var redis = require('redis')
+;
+
+function Rq(queueName, options) {
 
   this.host = options.host || '127.0.0.1';
   this.port = options.port || 6327;
-  this.queue = options.queue || '';
+  this.queue = queueName;
+  this._client = redis.createClient(this.port, this.host);
 
 }
+
+module.exports = exports = Rq;
---
+++ test/queue.test.js
@@ -0,0 +1,42 @@
+
+'use strict';
+
+var expect = require('chai').expect
+  , sinon = require('sinon')
+  , redis = require('redis')
+  , Queue = require('../src/queue')
+;
+
+describe('Queue', function() {
+
+  beforeEach(function() {
+
+    sinon.stub(redis, 'createClient').returns({});
+  });
+
+  afterEach(function() {
+
+    redis.createClient.restore();
+  });
+
+  describe('initialization', function() {
+
+    it('should initialize to defaults', function() {
+
+      var queue = new Queue('test', {});
+      expect(queue.host).to.equal('127.0.0.1');
+      expect(queue.port).to.equal(6327);
+    });
+
+    it('creates a new redis client with supplied options', function() {
+
+      var queue = new Queue('test', {host: 'foo', port: 1234 });
+
+      expect(queue.host).to.equal('foo');
+      expect(queue.port).to.equal(1234);
+      sinon.assert.calledWith(redis.createClient, 1234, 'foo');
+    
+    });
+  });
+
+});
